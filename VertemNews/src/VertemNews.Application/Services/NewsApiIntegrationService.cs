3442d9d5c2c553f96c9f090fa30c454c02711b67
Rogerio Sousa <rogerfloriano2013@gmail.com>
2023-06-13 11:31:09 -0300

    Alterações para conexão com NewsApi

--- VertemNews/src/VertemNews.Application/Services/NewsApiIntegrationService.cs
+++ VertemNews/src/VertemNews.Application/Services/NewsApiIntegrationService.cs
@@ -7,38 +7,38 @@ namespace VertemNews.Application.Services
 {
     public class NewsApiIntegrationService : INewsApiIntegrationService
     {
+        private HttpClient HttpClient;
+
+        public NewsApiIntegrationService()
+        {
+
+            HttpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+            HttpClient.DefaultRequestHeaders.Add("user-agent", "News-API-csharp/0.1");
+            HttpClient.DefaultRequestHeaders.Add("x-api-key", "d8a1cff5abef4e58bf89fae83f90a30a");
+        }
+
         public async Task<NewApiModel> GetNewsApiAsync()
         {
-            try
+            using (HttpClient client = new HttpClient())
             {
-                HttpClient client = new HttpClient();
-                var json = string.Empty;
-                var url = "https://newsapi.org/v2/everything?" +
-                          "q=Apple&" +
-                          "from=2023-05-26&" +
-                          "sortBy=popularity&" +
-                          "apiKey=d8a1cff5abef4e58bf89fae83f90a30a";
-
-                for (int i = 0; i < 10; i++)
+                try
                 {
-                    try
-                    {
-                        json = await client.GetStringAsync(url);
-                        break;
-                    }
-                    catch (Exception)
-                    {
-                        continue;
-                    }
+                    var json = string.Empty;
+                    var url = "https://newsapi.org/v2/everything?" +
+                              "q=Apple&" +
+                              "from=2023-05-26&" +
+                              "sortBy=popularity&" +
+                              "apiKey=d8a1cff5abef4e58bf89fae83f90a30a";
+
+                    json = await HttpClient.GetStringAsync(url);
+
+                    return JsonConvert.DeserializeObject<NewApiModel>(json);
                 }
-
-                if (json == null) return null;
-
-                return JsonConvert.DeserializeObject<NewApiModel>(json);
-            }
-            catch (Exception ex)
-            {
-                return null;
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"Erro: {ex.Message}");
+                    return null;
+                }
             }
         }
     }
